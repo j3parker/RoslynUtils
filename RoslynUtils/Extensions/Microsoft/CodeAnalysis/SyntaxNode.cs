25b814dad80251e857b2a988ce414a2498dd91f2
Jacob Parker <jacob@solidangle.ca>
2017-02-09 17:19:29 -0500

    Add SyntaxNode extensions for whitespace

---
+++ RoslynUtils/Extensions/Microsoft/CodeAnalysis/SyntaxNode.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace Microsoft.CodeAnalysis {
+	public static partial class _Extensions {
+		public static TSyntax WithLeadingSpace<TSyntax>( this TSyntax @this ) where TSyntax : SyntaxNode {
+			return @this.WithLeadingTrivia( SyntaxFactory.Whitespace( " " ) );
+		}
+
+		public static TSyntax WithTrailingSpace<TSyntax>( this TSyntax @this ) where TSyntax : SyntaxNode {
+			return @this.WithTrailingTrivia( SyntaxFactory.Whitespace( " " ) );
+		}
+
+		public static TSyntax WithSurroundingSpaces<TSyntax>( this TSyntax @this ) where TSyntax : SyntaxNode {
+			return @this.WithLeadingSpace().WithTrailingSpace();
+		}
+	}
+}
