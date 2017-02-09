09dc23c9b98e44443157fddc334272173f5cfc22
Jacob Parker <jacob@solidangle.ca>
2017-02-09 17:15:07 -0500

    Add FieldDeclarationSyntax extensions related to the readonly attribute

---
+++ RoslynUtils.Tests/Extensions/Microsoft/CodeAnalysis/CSharp/Syntax/FieldDeclarationSyntaxTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Microsoft.CodeAnalysis.CSharp.Syntax {
+	[TestFixture]
+	public sealed class FieldDeclarationSyntaxTests {
+		[Test]
+		public void AsReadOnly_MakesANonReadOnlyFieldReadOnly() {
+			var field = CSharpSyntaxTree.ParseText( "public int x = 3;" )
+				.GetRoot().DescendantNodes()
+				.OfType<FieldDeclarationSyntax>().Single();
+
+			Assert.IsFalse( field.IsReadOnly() );
+
+			var roField = field.AsReadOnly();
+
+			Assert.IsTrue( roField.IsReadOnly() );
+		}
+
+		[Test]
+		public void AsReadOnly_DoesntChangeReadOnlyFields() {
+			var field = CSharpSyntaxTree.ParseText( "public readonly int x = 3;" )
+				.GetRoot().DescendantNodes()
+				.OfType<FieldDeclarationSyntax>().Single();
+
+			Assert.IsTrue( field.IsReadOnly() );
+
+			var roField = field.AsReadOnly();
+
+			Assert.AreEqual( field, roField );
+		}
+
+		private static FieldDeclarationSyntax GetField( string text ) {
+			return CSharpSyntaxTree.ParseText( $"class Foo {{ {text}; }}" )
+				.GetRoot().DescendantNodes()
+				.OfType<FieldDeclarationSyntax>().Single();
+		}
+	}
+}
---
+++ RoslynUtils/Extensions/Microsoft/CodeAnalysis/CSharp/Syntax/FieldDeclarationSyntax.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Microsoft.CodeAnalysis.CSharp.Syntax {
+	public static partial class _Extensions {
+		private static readonly SyntaxToken ReadOnlySyntax = SyntaxFactory.Token(
+			SyntaxFactory.TriviaList(),
+			SyntaxKind.ReadOnlyKeyword,
+			SyntaxFactory.TriviaList( SyntaxFactory.Space )
+		);
+
+		public static bool HasModifier( this FieldDeclarationSyntax @this, SyntaxKind kind ) {
+			return @this.Modifiers.Any( m => m.Kind() == kind );
+		}
+
+		public static bool IsReadOnly( this FieldDeclarationSyntax @this ) {
+			return @this.HasModifier( SyntaxKind.ReadOnlyKeyword );
+		}
+
+		public static FieldDeclarationSyntax AsReadOnly( this FieldDeclarationSyntax @this ) {
+			if( @this.IsReadOnly() ) {
+				return @this;
+			}
+
+			var newModifiers = @this.Modifiers.Add( ReadOnlySyntax );
+
+			return @this.WithModifiers( newModifiers );
+		}
+	}
+}
